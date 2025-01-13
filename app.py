a2df25a8334e291566b7e34f742ad52e6e777819
agengprayoga02 <116251493+agengprayoga02@users.noreply.github.com>
2025-01-14 00:14:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ from itertools import product
 import sqlite3
 
 # Paths
+DATABASE_PATH = os.path.join(BASE_DIR, "dbd_prediction.db")
 DATASETS_DIR = os.path.join(BASE_DIR, "datasets")
 MODELS_DIR = os.path.join(BASE_DIR, "saved_models")
-DATABASE_PATH = os.path.join(BASE_DIR, "dbd_prediction.db")
+
 
 # SQLite Database Setup
 def init_db():
